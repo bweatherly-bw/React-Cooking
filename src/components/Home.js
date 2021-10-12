5cf692630ac16020628fdfafdd014aa7c01b6e8c
bweatherly-bw <brandon.weatherly58@gmail.com>
2021-10-12 05:19:01 -0400

    set up the home page to work as a search recipes. The Search bar works as intended, need to work on the styling and add some bootstrap to the application.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import Recipe from "./Recipe";
+import styles from './Styles.css';
 
-const Home = () => {
+function Home() {
+  const APP_ID = '33fe170b';
+  const APP_KEY = '52626f2cdf88d7903a4f69b64cdcd31d';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
-    <section className="Home">
-      <h1>This is the homepage</h1>
-   </section>
+    <section className="home">
+      <h1>Welcome to Brandon's Recipe Finder!</h1>
+      <p>Please use the Search Bar below to search for your next meal!</p>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch} />
+        <button className="search-button" type="submit"> Search </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients} />
+        ))}
+      </div>
+    </section>
   );
-};
+}
 
 export default Home;
